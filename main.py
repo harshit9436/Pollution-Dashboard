b9d75a5b1b35ff48b9bdff43b3ebdb247f93ab79
harshit9436 <harshit.9436@gmail.com>
2023-08-29 13:43:59 +0530

    athena connect

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 from fastapi import FastAPI
+import boto3
+import athena
 
 app = FastAPI()
 
+# Initialize the session as a global variable
+session = None
+
+def get_session():
+    global session
+    session = boto3.Session(aws_access_key_id="AKIAY6QBJLXT5A4D636O", aws_secret_access_key="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")
+
+# Use a decorator to ensure that get_session is called at startup
+@app.on_event("startup")
+async def startup_event():
+    get_session()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    global session
+    if session is None:
+        return {"message": "Session is not initialized"}
+    
+    params = {
+        'region': 'us-east-1',
+        'database': 'api_kolkata_glue_db',
+        'bucket': 'sachin-query-bucket',
+        'path': 'python',
+        'query': 'SELECT * FROM api_kolkata_glue_db.aqikolkata ORDER BY ts DESC LIMIT 5 ;'
+    }
+
+    location, data = athena.query_results(session, params)
+    print("Location:", location)
+    # print("Data:")
+    return data
+    # return {"message": "Hello"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ test.py
@@ -0,0 +1,23 @@
+import boto3
+import athena
+
+params = {
+    'region': 'us-east-1',
+    'database': 'api_kolkata_glue_db',
+    'bucket': 'sachin-query-bucket',
+    'path': 'python',
+    'query': 'SELECT * FROM api_kolkata_glue_db.aqikolkata ORDER BY ts DESC LIMIT 5 ;'
+}
+
+session = boto3.Session(aws_access_key_id = "AKIAY6QBJLXT5A4D636O",aws_secret_access_key = "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")
+# session.set_credentials()
+
+## Function for obtaining query results and location
+location, data = athena.query_results(session, params)
+print("Location:", location)
+print("Data:")
+
+print(data)
+
+# for i, d in enumerate(data):
+    # print('{}:'.format(i+1), d)
