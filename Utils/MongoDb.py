9047f4362301f1a2ff1e8deff0b8716a91003ff8
harshit9436 <harshit.9436@gmail.com>
2023-09-16 16:24:28 +0530

    authentication added, mongoDB added

---
+++ Utils/MongoDb.py
@@ -0,0 +1,79 @@
+import pymongo
+import Utils.Auth as Auth
+
+# Establish a connection to MongoDB (replace with your MongoDB URI)
+client = pymongo.MongoClient("mongodb+srv://harshit:harshit@cluster0.jlhfrqo.mongodb.net/")  # Replace with your MongoDB URI
+
+# Select a database (replace 'mydatabase' with your database name)
+db = client["Dashboard"]
+
+# Select a collection (replace 'users' with your collection name)
+collection = db["Users"]
+
+import uuid as uid  # Import the uuid library
+
+from pydantic import BaseModel
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class User:
+    def __init__(self, username, email, password, uuid=None):  # Provide a default value for uuid
+        self.username = username
+        self.email = email
+        self.password = password
+        self.uuid = uuid or str(uid.uuid4())  # Generate a UUID if not provided
+
+
+class UserManager:
+    @staticmethod
+    def insert_user(user):
+        # Convert the User object to a dictionary
+        password= Auth.get_password_hash(user.password)
+        user_dict = {
+            "username": user.username,
+            "email": user.email,
+            "hashed_password": password,
+            "uuid": user.uuid  # Include the uuid field
+        }
+        # Insert the user document into the collection
+        collection.insert_one(user_dict)
+
+    
+    @staticmethod
+    def find_user_by_username(username):
+        # Retrieve a user document by username, excluding the _id field
+        user_dict = collection.find_one({"username": username}, {"_id": 0})
+        if user_dict:
+            user_dict.pop("_id", None)  # Remove the _id field if it exists
+            return User(**user_dict)
+        return None
+
+    @staticmethod
+    def find_user_by_email(email):
+        # Retrieve a user document by email, excluding the _id field
+        user_dict = collection.find_one({"email": email}, {"_id": 0})
+        if user_dict:
+            user_dict.pop("_id", None)  # Remove the _id field if it exists
+            return User(**user_dict)
+        return None
+
+    @staticmethod
+    def verify_user_credentials(username, password):
+        # Verify user credentials (username and password)
+        user = UserManager.find_user_by_username(username)
+        if user and user.password == password:
+            return user
+        return None
+
+# Create a User object
+# new_user = User("ankit", "ankit@gmail.com", "ankit8632")
+
+# Insert the user into the database
+# UserManager.insert_user(new_user)
+
+user_by_username = UserManager.find_user_by_username("harshit")
+print(user_by_username.uuid)
+# user_by_email = UserManager.find_user_by_email("john@example.com")
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import boto3
-import Utils.utils as utils
+from datetime import timedelta
+from fastapi import Depends, FastAPI, HTTPException, Security, status
+from fastapi.security import (
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestForm,
+    SecurityScopes,
+)
+import Utils.Utils as Utils
 import athena
+import json
+import Utils.Auth as Auth
+from typing import Annotated
+import Utils.MongoDb as mongo
 
 app = FastAPI()
 
@@ -17,7 +28,9 @@ params = {
 
 def get_session():
     global session
-    session = boto3.Session(aws_access_key_id="AKIAY6QBJLXT5A4D636O", aws_secret_access_key="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")
+    with open("Utils/config.json", "r") as config_file:
+        config = json.load(config_file)
+    session = boto3.Session(aws_access_key_id=config["aws_access_key_id"], aws_secret_access_key=config["aws_secret_access_key"])
 
 # Use a decorator to ensure that get_session is called at startup
 @app.on_event("startup")
@@ -26,23 +39,62 @@ async def startup_event():
 
 @app.get("/")
 async def root():
-    global session
-    if session is None:
-        return {"message": "Session is not initialized"}
+    # global session
+    # if session is None:
+    #     return {"message": "Session is not initialized"}
     
-    params['query']= 'SELECT * FROM api_kolkata_glue_db.aqikolkata ORDER BY ts DESC LIMIT 5 ; '
+    # params['query']= 'SELECT * FROM api_kolkata_glue_db.aqikolkata ORDER BY ts DESC LIMIT 5 ; '
 
-    location, data = athena.query_results(session, params)
-    print("Location:", location)
-    return data
+    # location, data = athena.query_results(session, params)
+    # print("Location:", location)
+    # return data
+    return {"Message" : "hello"}
+
+
+@app.post("/token", response_model=Auth.Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = Auth.authenticate_user(Auth.fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    access_token_expires = timedelta(minutes=Auth.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = Auth.create_access_token(
+        data={"sub": user.username, "scopes": form_data.scopes},
+        expires_delta=access_token_expires,
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.post("/sign_up/")
+async def create_user(user_data: mongo.CreateUserRequest):
+    # Check if the username or email already exists
+    existing_user = mongo.UserManager.find_user_by_username(user_data.username)
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    
+    existing_email = mongo.UserManager.find_user_by_email(user_data.email)
+    if existing_email:
+        raise HTTPException(status_code=400, detail="Email already exists")
+    
+    # Create a User object
+    new_user = mongo.User(
+        username=user_data.username,
+        email=user_data.email,
+        password=user_data.password
+    )
+    
+    # Insert the user into the database
+    mongo.UserManager.insert_user(new_user)
+    
+    return {"message": "User created successfully"}
 
 @app.get("/sensors/{mac_id}/")
-async def get_sensor_data(mac_id: str, offset: str = "7"):
+async def get_sensor_data(current_user: Annotated[Auth.User, Depends(Auth.get_current_user)], mac_id: str, offset: str = "7"):
     global session
 
     # Calculate timestamps
-    ts_curr = utils.currentUnixTS()
-    ts_old = utils.offsetUnixTS(offset=int(offset))
+    ts_curr = Utils.currentUnixTS()
+    ts_old = Utils.offsetUnixTS(offset=int(offset))
     print(ts_old)
 
     # Build the SQL query
@@ -62,6 +114,58 @@ async def get_sensor_data(mac_id: str, offset: str = "7"):
     print(type(data))
     return data
 
+@app.post("/sensors/")
+async def get_sensor_data_from_body(     request: Request,
+    current_user: Auth.User = Depends(Auth.get_current_user) 
+):
+    global session
+
+    # Parse the JSON request body
+    sensor_data_request = await request.json()
+    mac_ids = sensor_data_request.get("mac_id", [])
+    offset = sensor_data_request.get("offset", 7)
+
+    # Calculate timestamps
+    ts_curr = Utils.currentUnixTS()
+    ts_old = Utils.offsetUnixTS(offset=offset)
+
+    # Build the SQL query
+    mac_id_filter = "', '".join(mac_ids)
+    query = f"""
+        SELECT * FROM api_kolkata_glue_db.aqikolkata
+        WHERE macid IN ('{mac_id_filter}') AND ts >= {ts_old} AND ts <= {ts_curr}
+        ORDER BY ts DESC LIMIT 5;
+    """
+
+    if session is None:
+        return {"message": "Session is not initialized"}
+
+    params["query"] = query
+
+    location, data = athena.query_results(session, params)
+    print("Location:", location)
+    print(type(data))
+    return data
+
+
+
+# @app.get("/users/me/", response_model=Auth.User)
+# async def read_users_me(
+#     current_user: Annotated[Auth.User, Depends(Auth.get_current_active_user)]
+# ):
+#     return current_user
+
+
+# @app.get("/users/me/items/")
+# async def read_own_items(
+#     current_user: Annotated[Auth.User, Security(Auth.get_current_active_user, scopes=["items"])]
+# ):
+#     return [{"item_id": "Foo", "owner": current_user.username}]
+
+
+# @app.get("/status/")
+# async def read_system_status(current_user: Annotated[Auth.User, Depends(Auth.get_current_user)]):
+#     return {"status": "ok"}
 
 
 
