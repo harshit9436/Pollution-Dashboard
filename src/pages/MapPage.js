7d9bedf5e4e482d2590ada9fe9ccfe47bb88efdd
harshit9436 <harshit.9436@gmail.com>
2023-09-29 00:17:25 +0530

    pop up

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -6,12 +6,12 @@ import axios from 'axios';
 
 const activeIconStyle = {
   iconUrl: '/assets/icons/green.svg',
-  iconSize: [38, 38],
+  iconSize: [50, 50],
 };
 
 const inactiveIconStyle = {
   iconUrl: '/assets/icons/red.svg',
-  iconSize: [38, 38],
+  iconSize: [50, 50],
 };
 
 const customIcon = L.icon(activeIconStyle);
@@ -24,89 +24,91 @@ const styles = {
     padding: 0,
   },
   '#root': {
-    height: '100vh',
+    height: '85vh',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'column',
-    // gap: '1em',
-    
+    gap: '1em',
     fontFamily: 'sans-serif',
-    fontSize : '100 rem',
-    color: '#000000',
+    color: '#333',
+    top: '10',
     backgroundColor: '#f3e4d7',
   },
   '.leaflet-container': {
-    height: '100%',
+    flex: 1,
     width: '100%',
-    borderRadius: '2rem',
   },
   '.marker-cluster': {
-    width: "30px",
-    height: "30px",
-    fontSize : "15px"
-    // text-align: "center",
-    // border-radius: 15px;
-    // font: 12px "Helvetica Neue", Arial, Helvetica, sans-serif;
-    },
+    width: '30px',
+    height: '30px',
+    fontSize: '15px',
+  },
 };
 
 export default function App() {
   const [markersData, setMarkersData] = useState([]);
-
+  const [lastRefreshTime, setLastRefreshTime] = useState(new Date());
   useEffect(() => {
     // Define the URL of your backend API
     const apiUrl = 'http://127.0.0.1:8000/list/sensors';
 
     // Use Axios to make a GET request to the API
-    axios.get(apiUrl)
+    axios
+      .get(apiUrl)
       .then((response) => {
         // Assuming the response data is an array of markers
         const formattedMarkers = response.data
-          .filter(data => data.lat !== 0 && data.long !== 0) // Filter out data with lat or long equal to 0
+          .filter((data) => data.lat !== 0 && data.long !== 0) // Filter out data with lat or long equal to 0
           .map((data, index) => ({
             geocode: [data.lat, data.long],
-            popUp: data.is_active ? 'Active' : 'Inactive',
+            // popUp: data.is_active ? data.avg2_5 : 'Inactive',
+            popUp: `MAC ID: ${data.macid}\n Avg 2.5: ${data.avg2_5.toFixed(2)}\nAvg 10.0: ${data.avg10_0.toFixed(2)}`,
             icon: data.is_active ? L.icon(activeIconStyle) : L.icon(inactiveIconStyle),
-
           }));
 
         // Set the formatted markers in the state
-        setMarkersData(formattedMarkers);
-
-        // Set the formatted markers in the state
-        console.log("retrieved data");
+        console.log('retrieved data');
         console.log(formattedMarkers);
         setMarkersData(formattedMarkers);
       })
       .catch((error) => {
         console.error('Error fetching markers:', error);
       });
+
+    // Set up an interval to refresh the page every 15 seconds
+    const refreshInterval = setInterval(() => {
+      console.log({lastRefreshTime});
+      setLastRefreshTime(new Date());
+      window.location.reload(); // Reload the page
+    }, 900000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(refreshInterval);
   }, []);
 
   const createClusterCustomIcon = function (cluster) {
     return L.divIcon({
-      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+      html: `
+        <div class="cluster-icon">
+          <img src="/assets/icons/clusterIcon.svg" />
+          <div class="cluster-count" style="font-size: 22px; color: black;">${cluster.getChildCount()}</div>
+          </div>
+      `,
       className: 'custom-marker-cluster',
-      iconSize: [33, 33],
-      iconAnchor: [16, 32],
+      iconSize: [4, 4], // Adjust the icon size here
+
+      // iconAnchor: [20, 20], // Adjust the anchor point for proper positioning
       backgroundColor: '#333',
-      height: '100%',
-      width: '100%',
       color: '#fff',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      // borderRadius: '50%',
-      // fontSize: '50 rem',
       boxShadow: '0 0 0px 5px #fff',
     });
   };
-
+  
   return (
     <div style={styles['*']}>
       <div id="root" style={styles['#root']}>
-        <MapContainer center={[22.572, 88.364]} zoom={10} style={styles['.leaflet-container']}>
+        <MapContainer center={[22.572, 88.364]} zoom={12} style={styles['.leaflet-container']}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
