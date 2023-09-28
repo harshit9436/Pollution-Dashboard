bf48c031a47172fb9ef5fc9547d6b98efec0bcf9
harshit9436 <harshit.9436@gmail.com>
2023-09-28 21:57:52 +0530

    map.js changed

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,112 +1,124 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
 import L from 'leaflet';
+import axios from 'axios';
 
-// Create custom icon
-const customIconStyle = {
-  // iconUrl: require('./logo.svg'),
-  iconUrl: './logo.svg',
-  iconSize: [38, 38], // size of the icon
+const activeIconStyle = {
+  iconUrl: '/assets/icons/green.svg',
+  iconSize: [38, 38],
 };
-const customIcon = L.icon(customIconStyle);
+
+const inactiveIconStyle = {
+  iconUrl: '/assets/icons/red.svg',
+  iconSize: [38, 38],
+};
+
+const customIcon = L.icon(activeIconStyle);
+
 const styles = {
+  // Your styles here
   '*': {
     boxSizing: 'border-box',
     margin: 0,
     padding: 0,
   },
   '#root': {
-    height: '85vh',
+    height: '100vh',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'column',
-    gap: '1em',
+    // gap: '1em',
+    
     fontFamily: 'sans-serif',
-    color: '#333',
-    top:'10',
+    fontSize : '100 rem',
+    color: '#000000',
     backgroundColor: '#f3e4d7',
   },
   '.leaflet-container': {
-    // height: '90vh',
-    flex:1,
+    height: '100%',
     width: '100%',
-    // borderRadius: '2rem',
-    // alignItems:'centre',
-    // position: 'absolute',
-    // right: '10',
-    // bottom: '10',
+    borderRadius: '2rem',
   },
-};
-
-const markers = [
-  {
-    geocode: [48.86, 2.3522],
-    popUp: 'Hello, I am pop up 1',
-  },
-  {
-    geocode: [48.85, 2.3522],
-    popUp: 'Hello, I am pop up 2',
-  },
-  {
-    geocode: [48.855, 2.34],
-    popUp: 'Hello, I am pop up 3',
-  },
-];
-const createClusterCustomIcon =(cluster) =>{
-  return L.divIcon({
-    html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
-    className: 'custom-marker-cluster',
-    iconSize: [33, 33],
-    iconAnchor: [16, 32], // Adjust the anchor point if needed
-    backgroundColor: '#333',
-    height: '2em',
-    width: '2em',
-    color: '#fff',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: '50%',
-    fontSize: '1.2rem',
-    boxShadow: '0 0 0px 5px #fff',
-  });
+  '.marker-cluster': {
+    width: "30px",
+    height: "30px",
+    fontSize : "15px"
+    // text-align: "center",
+    // border-radius: 15px;
+    // font: 12px "Helvetica Neue", Arial, Helvetica, sans-serif;
+    },
 };
 
 export default function App() {
+  const [markersData, setMarkersData] = useState([]);
+
+  useEffect(() => {
+    // Define the URL of your backend API
+    const apiUrl = 'http://127.0.0.1:8000/list/sensors';
+
+    // Use Axios to make a GET request to the API
+    axios.get(apiUrl)
+      .then((response) => {
+        // Assuming the response data is an array of markers
+        const formattedMarkers = response.data
+          .filter(data => data.lat !== 0 && data.long !== 0) // Filter out data with lat or long equal to 0
+          .map((data, index) => ({
+            geocode: [data.lat, data.long],
+            popUp: data.is_active ? 'Active' : 'Inactive',
+            icon: data.is_active ? L.icon(activeIconStyle) : L.icon(inactiveIconStyle),
+
+          }));
+
+        // Set the formatted markers in the state
+        setMarkersData(formattedMarkers);
+
+        // Set the formatted markers in the state
+        console.log("retrieved data");
+        console.log(formattedMarkers);
+        setMarkersData(formattedMarkers);
+      })
+      .catch((error) => {
+        console.error('Error fetching markers:', error);
+      });
+  }, []);
+
+  const createClusterCustomIcon = function (cluster) {
+    return L.divIcon({
+      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+      className: 'custom-marker-cluster',
+      iconSize: [33, 33],
+      iconAnchor: [16, 32],
+      backgroundColor: '#333',
+      height: '100%',
+      width: '100%',
+      color: '#fff',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      // borderRadius: '50%',
+      // fontSize: '50 rem',
+      boxShadow: '0 0 0px 5px #fff',
+    });
+  };
+
   return (
     <div style={styles['*']}>
       <div id="root" style={styles['#root']}>
-        <MapContainer center={[22.572645, 88.363892]} zoom={15} style={styles['.leaflet-container']}>
-          {/* OPEN STREEN MAPS TILES */}
+        <MapContainer center={[22.572, 88.364]} zoom={10} style={styles['.leaflet-container']}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {/* <TileLayer
-          attribution="Google Maps"
-          // url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
-          url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
-          // url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
-          maxZoom={20}
-          subdomains={["mt0", "mt1", "mt2", "mt3"]}
-          /> */}
+
           <MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon}>
-            {markers.map((marker, index) => (
-              <Marker key={index} position={marker.geocode} icon={customIcon}>
+            {markersData.map((marker, index) => (
+              <Marker key={index} position={marker.geocode} icon={marker.icon}>
                 <Popup>{marker.popUp}</Popup>
               </Marker>
             ))}
-            <Marker position={[22.505, 88.09]} icon={customIcon}>
-              <Popup>This is popup 1</Popup>
-            </Marker>
-            <Marker position={[22.504, 88.1]} icon={customIcon}>
-              <Popup>This is popup 2</Popup>
-            </Marker>
-            <Marker position={[22.5, 88.09]} icon={customIcon}>
-              <Popup>This is popup 3</Popup>
-            </Marker>
-        </MarkerClusterGroup>
+          </MarkerClusterGroup>
         </MapContainer>
       </div>
     </div>
