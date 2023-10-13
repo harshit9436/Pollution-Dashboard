6a9f0c2d8db0b5819231d67e5340da01f8591974
ankit8632 <raushanankitdbg@gmail.com>
2023-10-14 00:10:12 +0530

    Resolved conflict in package-lock.json, keeping local changes

--- src/components/card/SimpleCard.js
+++ src/components/card/SimpleCard.js
@@ -1,95 +1,32 @@
-import PropTypes from 'prop-types';
-import React, { useState, useEffect } from 'react';
-import ReactApexChart from 'react-apexcharts';
-import { Card, CardHeader, Box } from '@mui/material';
-import axios from 'axios';
-import { format } from 'date-fns';
-import { useChart } from '../../components/chart'; 
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-
-SimpleCard.propTypes = {
-  title: PropTypes.string,
-  subheader: PropTypes.string,
-  macid: PropTypes.string.isRequired,
-};
-
-export default function SimpleCard({ title, subheader, macid, ...other }) {
-  const [pm2_5Data, setPm2_5Data] = useState([]); 
-  const [pm10_0Data, setPm10_0Data] = useState([]);
-  const [ts, set_ts] = useState([]);
-  
-  const encodedMacId = encodeURIComponent(macid);
-  const fetchSensorData = async () => {
-    try {
-      const apiUrl = `http://127.0.0.1:8000/sensors/${encodedMacId}/?offset=7`;
-      const response = await axios.get(apiUrl);
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching sensor data:', error);
-      return null;
-    }
-  };
-
-  useEffect(() => {
-    fetchSensorData().then(data => {
-      if (data) {
-        const pm2_5Data = data.map(point => parseFloat(point.pm2_5));
-        const pm10_0Data = data.map(point => parseFloat(point.pm10_0));
-        const formattedTimestamps = data.map(point => {
-          const unixTimestamp = parseInt(point.ts) * 1000;
-          const formattedDate = format(new Date(unixTimestamp), 'MM/dd/yyyy HH:mm:ss');
-          return formattedDate;
-        });
-
-        setPm2_5Data(pm2_5Data);
-        setPm10_0Data(pm10_0Data);
-        set_ts(formattedTimestamps);
-      }
-    });
-  }, [macid]);
-
-  const chartLabels = ts;
-  const chartData = [
-    {
-      name: 'PM 2.5',
-      type: 'line',
-      fill: 'solid',
-      data: pm2_5Data,
-    },
-    {
-      name: 'PM 10',
-      type: 'line',
-      fill: 'solid',
-      data: pm10_0Data,
-    },
-  ];
-
-  const chartOptions = useChart({
-    plotOptions: { bar: { columnWidth: '16%' } },
-    fill: { type: chartData.map((i) => i.fill) },
-    labels: chartLabels,
-    xaxis: { type: 'datetime' },
-    tooltip: {
-      shared: true,
-      intersect: false,
-      y: {
-        formatter: (y) => {
-          if (typeof y !== 'undefined') {
-            return `${y.toFixed(0)} visits`;
-          }
-          return y;
-        },
-      },
-    },
-  });
-
+export default function MediaCard() {
   return (
-    <Card {...other}>
-      <CardHeader title={title} subheader={subheader} />
-
-      <Box sx={{ p: 3, pb: 1 }} dir="ltr">
-        <ReactApexChart type="line" series={chartData} options={chartOptions} height={364} />
-      </Box>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
     </Card>
   );
 }
